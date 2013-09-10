689bc3016cdd94e74b256003f8fc7635ca88822c
Kamarul Ariffin Ismail <kamarul.ismail@gmail.com>
2013-09-10 19:15:22 +0800

    update unit test

--- v1/classes/Logger.php
+++ v1/classes/Logger.php
@@ -6,7 +6,7 @@ class Logger
 {
 
     const MAX_LOG_MESSAGE_LENGTH = 500;
-    
+
     # LOG_EMERG = 0;
     # LOG_ALERT = 1;
     # LOG_CRIT = 2;
@@ -15,13 +15,13 @@ class Logger
     # LOG_NOTICE = 5;
     # LOG_INFO = 6;
     # LOG_DEBUG = 7;
-    
+
     const DAILY_LOG = 0;
     const PERMANENT_LOG = 1;
-    
+
     public static $log_level = LOG_NOTICE;
     public static $error_message = '';
-    
+
     public static function setLogLevel($log_level)
     {
         if (($log_level< LOG_EMERG) or ($log_level > LOG_DEBUG))
@@ -29,32 +29,32 @@ class Logger
 	    self::$error_message = 'Invalid log level';
             return false;
         }
-        
+
         self::$log_level = $log_level;
 
 	return true;
     }
-    
-    public static function logMessage($filename, $message, $log_level = LOG_NOTICE, $destination = self::DAILY_LOG)      
+
+    public static function logMessage($filename, $message, $log_level = LOG_NOTICE, $destination = self::DAILY_LOG)
     {
         if (($destination != self::DAILY_LOG) and ($destination != self::PERMANENT_LOG))
         {
 	    self::$error_message = 'Invalid destination';
             return false;
         }
-        
+
         if (($log_level< LOG_EMERG) or ($log_level > LOG_DEBUG))
         {
 	    self::$error_message = 'Invalid log level';
             return false;
         }
-        
+
         if ( (strlen($message) == 0) or (strlen($message) > self::MAX_LOG_MESSAGE_LENGTH) )
         {
 	    self::$error_message = 'Invalid message lenght';
             return false;
         }
-               
+
         # Minimum 5 characters, maximum 50 characters. Must start with an alphanumeric character and may only contain alphanumeric, period (.), dash (-) and underscore (_).
 	$valid_filename = preg_match('/^([A-Z]|[a-z]|[0-9]){1}([A-Z]|[a-z]|[0-9]|\.|-|_){4,49}$/',$filename);
 	if ( ($valid_filename === false) or ($valid_filename == 0) )
@@ -62,7 +62,7 @@ class Logger
 	   self::$error_message = 'Invalid filename';
            return false;
 	}
-	        
+
         if ($log_level > self::$log_level)
         {
             return true;
@@ -73,7 +73,7 @@ class Logger
 	    self::$error_message = 'Can not open log';
 	    return false;
 	}
-	
+
 	if (!syslog($log_level, $message))
 	{
 	    self::$error_message = 'Can not send message to syslog';
@@ -86,6 +86,6 @@ class Logger
 
 	return true;
     }
-        
+
 }
 ?>
--- v1/tests/LoggerTest.php
+++ v1/tests/LoggerTest.php
@@ -5,12 +5,31 @@
  *
  * @author Kamarul Ariffin Ismail <kamarul.ismail@gmail.com>
  */
+require_once realpath(dirname(__FILE__) . '/../') . '/classes/Logger.php';
+
 class LoggerTest extends PHPUnit_Framework_TestCase {
     public function testEmpty() {
         $stack = array();
         $this->assertEmpty($stack);
 
         return $stack;
+    }
+
+    public function testSetLevel(){
+        $class = $this->getMockClass(
+                'Sam\\Logger', /* name of class to mock     */
+                array('setLogLevel') /* list of methods to mock   */
+        );
+
+        $class::staticExpects($this->any())
+                ->method('setLogLevel')
+                ->will($this->returnValue(1));
+
+        $this->assertEquals(true, $class::setLogLevel(1));
+    }
+
+    public function testLogLevelVariable(){
+        $this->assertEquals(false, Sam\Logger::setLogLevel(11));
     }
 }
 
